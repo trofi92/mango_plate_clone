4f598a256d2a73825440159cc7edacc164f4b3e1
trofi92 <lunaslash9249@gmail.com>
2023-03-08 14:24:24 +0900

    env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -13,17 +13,6 @@ export const firebaseConfig = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyB3sy3DYAb3sz7WZgEQrtkwANzizj3wBnI",
-//   authDomain: "bananaplate-clone.firebaseapp.com",
-//   projectId: "bananaplate-clone",
-//   storageBucket: "bananaplate-clone.appspot.com",
-//   messagingSenderId: "339115685729",
-//   appId: "1:339115685729:web:71a7f7f82f4df7331cf720",
-//   databaseURL:
-//     "https://bananaplate-clone-default-rtdb.firebaseio.com/",
-//   measurementId: "G-LXSTVR58XR",
-// };
 
 const app = initializeApp(firebaseConfig);
 
